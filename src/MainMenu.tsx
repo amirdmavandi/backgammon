877e49af4e58d62c076033b4addfd315ad05cca6
amirdmavandi <amdm354@gmail.com>
2025-12-04 18:30:59 +0330

    Refactor MainMenu component and cleanup imports

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -1,67 +1,58 @@
-import { FunctionComponent, useState } from "react";
+    import { FunctionComponent, useState, useEffect } from "react";  
+import { createLobby } from "./Firebase";  
+import { useNavigate } from "react-router-dom";  
+import { useAppDispatch, useAppSelector } from "./store/hooks";  
+import { setShowMatchSetupScreen } from "./store/settingsSlice";  
+import { setWipeTransition } from "./store/wipeTransitionSlice";  
 
-import { createLobby } from "./Firebase";
-import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "./store/hooks";
-import { resetLocalStore } from "./Utils";
-import { setShowMatchSetupScreen } from "./store/settingsSlice";
-import { setWipeTransition } from "./store/wipeTransitionSlice";
+type MainMenuProps = {};  
 
-type MainMenuProps = {};
+const MainMenu: FunctionComponent<MainMenuProps> = () => {  
+  const navigate = useNavigate();  
+  const dispatch = useAppDispatch();  
+  const coins = useAppSelector(state => state.playerCoins.playerOne); // تعداد سکه کاربر  
+  const [isCreatingMultiplayerLobby, setIsCreatingMultiplayerLobby] = useState(false);  
 
-const MainMenu: FunctionComponent<MainMenuProps> = () => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+  const createOnlineLobby = async () => {  
+    if (isCreatingMultiplayerLobby) return;  
+    if (coins < 30) {  
+      alert("You need at least 30 coins to play online.");  
+      return;  
+    }  
+    setIsCreatingMultiplayerLobby(true);  
+    const createLobbyResult = await createLobby();  
+    dispatch(setShowMatchSetupScreen(true));  
+    dispatch(setWipeTransition(true));  
+    navigate("/" + createLobbyResult.roomCode);  
+  };  
 
-  const [isCreatingMultiplayerLobby, setIsCreatingMultiplayerLobby] =
-    useState(false);
+  let onlineButtonOrSpinner = (  
+    <div className="Online-multiplayer-button" onClick={createOnlineLobby}>  
+      Play Online  
+    </div>  
+  );  
+  if (isCreatingMultiplayerLobby) {  
+    onlineButtonOrSpinner = (  
+      <div className="Online-multiplayer-button-spinner">Loading...</div>  
+    );  
+  }  
 
-  const createLocalGame = () => {
-    if (isCreatingMultiplayerLobby) {
-      return;
-    }
-    resetLocalStore(dispatch, true);
-    dispatch(setShowMatchSetupScreen(true));
-    dispatch(setWipeTransition(true));
-    navigate("/local");
-  };
-
-  const createOnlineLobby = async () => {
-    if (isCreatingMultiplayerLobby) {
-      return;
-    }
-    setIsCreatingMultiplayerLobby(true);
-    const createLobbyResult = await createLobby();
-    dispatch(setShowMatchSetupScreen(true));
-    dispatch(setWipeTransition(true));
-    navigate("/" + createLobbyResult.roomCode);
-  };
-
-  let onlineButtonOrSpinner = (
-    <div className={"Online-multiplayer-button"} onClick={createOnlineLobby} />
-  );
-  if (isCreatingMultiplayerLobby) {
-    onlineButtonOrSpinner = (
-      <div className={"Online-multiplayer-button-spinner"} />
-    );
-  }
-
-  return (
-    <div className={"Main-menu-wrapper"}>
-      <div className={"Title-wrapper"}>
-        <div className={"Title-text"}></div>
-      </div>
-      <div className={"Menu-button-wrapper"}>
-        <div className={"Local-multiplayer-button"} onClick={createLocalGame} />
-        {onlineButtonOrSpinner}
-      </div>
-      <div className={"Credits-text"}>
-        <div>Backgammon v 1.0.2.574dced</div>
-        <div>by Sam Swarr (sam-swarr.github.io)</div>
-        <div>font Barlow by Jeremy Tribby</div>
-      </div>
-    </div>
-  );
-};
+  return (  
+    <div className="Main-menu-wrapper">  
+      <div className="Title-wrapper">  
+        <div className="Title-text">Backgammon Online</div>  
+      </div>  
+      <div className="Menu-button-wrapper">  
+        {onlineButtonOrSpinner}  
+      </div>  
+      <div className="Credits-text">  
+        <div>Backgammon v 1.0.2.574dced</div>  
+        <div>by Sam Swarr (sam-swarr.github.io)</div>  
+        <div>font Barlow by Jeremy Tribby</div>  
+      </div>  
+      <div className="Coins-info">Coins: {coins}</div>
+    </div>  
+  );  
+};  
 
 export default MainMenu;
