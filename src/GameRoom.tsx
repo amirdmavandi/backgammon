df1f717f134ed5102834daeec4ae4712f112c3a1
amirdmavandi <amdm354@gmail.com>
2025-12-06 03:40:52 +0330

    Refactor GameRoom component and update state usage

--- src/GameRoom.tsx
+++ src/GameRoom.tsx
@@ -1,18 +1,18 @@
-      import { FunctionComponent, useEffect } from "react";
+           import { FunctionComponent, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import GameBoard from "./GameBoard";
 import { Player } from "./Types";
 import PlayerCard, { PlayerCardSide } from "./PlayerCard";
 import SettingsMenu from "./SettingsMenu";
 import GameOverDialog from "./GameOverDialog";
-import { RootState } from "./store"; // مسیر درست به store
+import { RootState } from "./store"; 
 import { handleGameOverCoins } from "./store/playerCoinsSlice";
 
 type GameRoomProps = {
   playerPerspective: Player;
-  playerOneScore: number; // امتیاز بازیکن 1
-  playerTwoScore: number; // امتیاز بازیکن 2
-  stakeAmount: number; // مقدار سکه شرط‌بندی
+  playerOneScore: number;
+  playerTwoScore: number;
+  stakeAmount: number; // همیشه 10 سکه به وسط منتقل می‌کنیم
 };
 
 const GameRoom: FunctionComponent<GameRoomProps> = ({
@@ -22,7 +22,7 @@ const GameRoom: FunctionComponent<GameRoomProps> = ({
   stakeAmount,
 }: GameRoomProps) => {
   const dispatch = useDispatch();
-  const coins = useSelector((state: RootState) => state.playerCoins);
+  const pot = useSelector((state: RootState) => state.pot.coins);
 
   useEffect(() => {
     // وقتی بازی تمام شد، بررسی برنده و انتقال سکه
@@ -54,7 +54,7 @@ const GameRoom: FunctionComponent<GameRoomProps> = ({
   }, [playerOneScore, playerTwoScore, stakeAmount, dispatch]);
 
   return (
-    <div className={"Game-area-wrapper"}>
+    <div className="Game-area-wrapper">
       <SettingsMenu playerPerspective={playerPerspective} />
       <GameOverDialog playerPerspective={playerPerspective} />
       <PlayerCard
@@ -70,12 +70,8 @@ const GameRoom: FunctionComponent<GameRoomProps> = ({
         side={PlayerCardSide.Bottom}
         playerPerspective={playerPerspective}
       />
-      <div style={{ marginTop: "1rem" }}>
-        <h2>Player 1 Coins: {coins.playerOne}</h2>
-        <h2>Player 2 Coins: {coins.playerTwo}</h2>
-      </div>
     </div>
   );
 };
 
-export default GameRoom;
+export default GameRoom; 
